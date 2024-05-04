d203d2df000a66bcf02c91f80ce631e62fa02b75
kashishbhandula <kashishbhandula112@gmail.com>
2024-05-04 16:31:23 +0530

    initialised folder structure

---
+++ src/components/jobs_search/jobsSearch.js
@@ -0,0 +1,1 @@
+export default function JobsSearch(){return<>hi</>}
---
+++ src/pages/jobs_search/index.js
@@ -0,0 +1,5 @@
+import JobsSearch from "../../components/jobs_search/jobsSearch"
+import "../../styles/jobs_search.css"
+export default function JobsSearchPage() {
+  return<JobsSearch></JobsSearch>
+}
