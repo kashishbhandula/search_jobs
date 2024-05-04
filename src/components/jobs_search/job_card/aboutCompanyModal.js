cf5905c50dc56684f4ac7d0aa6c0f2b78d54ec65
kashishbhandula <kashishbhandula112@gmail.com>
2024-05-05 01:48:46 +0530

    fixed css

--- src/components/jobs_search/job_card/aboutCompanyModal.js
+++ src/components/jobs_search/job_card/aboutCompanyModal.js
@@ -1,9 +1,13 @@
-export default function AboutCompanyModal({viewMore, setViewMore }) {
+export default function AboutCompanyModal({ viewMore, setViewMore }) {
   return (
     viewMore && (
       <div>
-        <div className="modal-overlay" onClick={()=>setViewMore(null)}></div>
-        <div className="about_company_modal">{viewMore}</div>
+        <div className="modal-overlay" onClick={() => setViewMore(null)}></div>
+        <div className="about_company_modal">
+          <h3 className="job_description_title">Job Description</h3>
+          <div className="section_title"> About Company:</div>
+          {viewMore}
+        </div>
       </div>
     )
   );
